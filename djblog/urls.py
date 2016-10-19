d744041bd98eb4aab002e5fc041f836f7d64521e
Vitalii <kuzvitalii@gmail.com>
2016-10-19 15:01:31 +0000

    Разобрался в косяке и закончил вновь главу 11

--- djblog/urls.py
+++ djblog/urls.py
@@ -20,13 +20,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from registration.backends.simple.views import RegistrationView
 
 from rango import views
-
-class MyRegistrationView(RegistrationView):
-    def get_success_url(self, user):
-        return '/rango/'
 
 
 
@@ -37,6 +32,3 @@ urlpatterns = [
     url(r'^accounts/', include('registration.backends.simple.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-url(r'^accounts/register/$',
-    MyRegistrationView.as_view(),
-        name='registration_register'),
